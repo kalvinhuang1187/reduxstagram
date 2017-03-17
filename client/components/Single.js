9acd2d5cfea254cf12c3989cfebb36a2b2f13132
Kalvin Huang <kalvin_huang@yahoo.com>
2017-03-16 22:57:29 -0700

    Displaying and adding comments

--- client/components/Single.js
+++ client/components/Single.js
@@ -6,16 +6,18 @@ const Single = React.createClass({
   render() {
     // need index of the post
     // get us the post
+    const { postId } = this.props.params;
+
     const i = this.props.posts.findIndex((post) => 
-      post.code === this.props.params.postId);
+      post.code === postId);
     const post = this.props.posts[i];
-    console.log(i);
-    console.log(post);
+
+    const postComments = this.props.comments[postId] || [];
     
     return (
       <div className="single-photo">
         <Photo i={i} post={post} {...this.props} />
-        <Comments />
+        <Comments postComments={postComments}/>
       </div>
     )
   }
